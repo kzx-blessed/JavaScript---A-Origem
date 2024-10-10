ef6b15464ac33768826070df0c297e016a9a1fe4
kzx-blessed <kalebe.msantana@gmail.com>
2024-10-09 21:41:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function creator() { 
+    alert("Brendan Eich é um programador de computadores americano e criador da linguagem de programação JavaScript.");
+}
+
+function c1() {
+    alert("Javascript é uma linguagem de programação leve que os desenvolvedores usam para criar interações mais dinâmicas ao desenvolver páginas da Web, aplicativos, servidores, jogos, etc. ")
+}
+
+function c2() {
+    alert("Concebido como uma linguagem do lado do servidor por Brendan Eich (então empregado pela Netscape Corporation), o JavaScript logo chegou ao Netscape Navigator 2.0 em setembro de 1995.")
+}
+
+function c3() {
+    alert("o JavaScript foi originalmente chamado de Mocha, depois LiveScript e só então o nome JavaScript foi escolhido, porque a Netscape, empresa criadora da linguagem, queria ganhar fama em cima do sucesso do Java.")
+}
