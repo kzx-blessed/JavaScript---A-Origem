7117e67bee5e91668bd913c2956b7049e01dd246
kzx-blessed <kalebe.msantana@gmail.com>
2024-10-30 22:57:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,54 @@ function c2() {
 function c3() {
     alert("o JavaScript foi originalmente chamado de Mocha, depois LiveScript e só então o nome JavaScript foi escolhido, porque a Netscape, empresa criadora da linguagem, queria ganhar fama em cima do sucesso do Java.")
 }
+
+const item1 = document.getElementById("item1");
+const button = document.getElementById("changeColorBtn");
+
+    item1.style.backgroundColor = "rgb(20, 23, 37)";
+    changeColorBtn.style.backgroundColor = "blue";
+
+function mudarCorDeFundo() {
+
+    if (item1.style.backgroundColor === "rgb(20, 23, 37)" && changeColorBtn.style.backgroundColor === "blue") {
+
+      item1.style.transition = "background-color 1s";
+      changeColorBtn.style.transition = "background-color 0.5s";
+
+       item1.style.backgroundColor = "red";
+       changeColorBtn.style.backgroundColor = "rgb(117, 23, 16)";
+
+    } else {
+
+      item1.style.transition = "background-color 1s";
+      changeColorBtn.style.transition = "background-color 0.5s";
+
+      item1.style.backgroundColor = "rgb(20, 23, 37)";
+      changeColorBtn.style.backgroundColor = "blue";
+    }
+
+  }
+
+  let count = 0;
+
+    document.getElementById('clickCounterBtn').addEventListener('click', function() {
+    count++;
+    document.getElementById('counter').textContent = "Cliques: " + count;
+});
+
+const textToToggle = document.getElementById('textToToggle');
+const toggleTextBtn = document.getElementById('toggleTextBtn');
+
+toggleTextBtn.addEventListener('click', function() {
+if (textToToggle.style.display === 'none') {
+textToToggle.style.display = 'block';
+toggleTextBtn.textContent = 'Esconder';
+} else {
+textToToggle.style.display = 'none';
+toggleTextBtn.textContent = 'Mostrar';
+}
+});
+
+function mudarTexto() {
+    document.getElementById('titulo').innerText = 'Texto alterado pelo JavaScript!';
+    }
